8b3a1c6dbcbb56c92ae1a2d5be27f7df92400f59
Michael Gerstmann <mg@bytepark.de>
2014-09-22 15:50:13 +0200

    add initial db setup form

---
+++ app/admin/modules/form-setup.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mg
+ * Date: 22.09.2014
+ * Time: 04:45
+ */
+?>
+
+<form>
+    <fieldset>
+        <legend>Installationsdaten</legend>
+        <label for="setup-db-databasename">Datenbankname</label>
+        <input type="text" name="setup-db-databasename" id="setup-db-databasename" required="required">
+
+
+        <input type="submit" name="setup-db-submit" id="setup-db-submit" value="Weiter">
+
+    </fieldset>
+</form>
