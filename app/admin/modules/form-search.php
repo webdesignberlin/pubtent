2ed5be8288feb2b65dade21d044cbe89b3a29aa0
Michael Gerstmann <mg@bytepark.de>
2014-09-22 14:13:12 +0200

    add search module

---
+++ app/admin/modules/form-search.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mg
+ * Date: 22.09.2014
+ * Time: 03:42
+ */
+?>
+
+<form>
+    <fieldset>
+        <legend>Suche</legend>
+        <label for="site-search-field">Suchbegriff eingeben um das Dashboard zu durchsuchen</label>
+        <input type="search" name="site-search-field" id="site-search-field" required="required">
+
+        <label for="site-search-submit">Suche starten</label>
+        <input type="submit" name="site-search-submit" id="site-search-submit" value="Suchen">
+
+    </fieldset>
+</form>
