ae0d3ea2b8065aeaaa5f722468b1368bf648906d
Michael Gerstmann <mg@bytepark.de>
2014-09-22 15:43:45 +0200

    add standard table template

---
+++ app/admin/modules/table.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mg
+ * Date: 22.09.2014
+ * Time: 04:39
+ */
+?>
+
+<table>
+    <thead>
+        <tr>
+            <th colspan="2">Tableheader</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Standard Cell 1</td>
+            <td>Standard Cell 2</td>
+        </tr>
+        <tr>
+            <th>Header Cell 1</th>
+            <td>Standard Cell 2</td>
+        </tr>
+    </tbody>
+    <tfoot>
+        <tr>
+            <td>Table Footer</td>
+        </tr>
+    </tfoot>
+</table>
