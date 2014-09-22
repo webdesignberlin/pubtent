6b9db8806b9eea00bf6726c84585725a73104af5
Michael Gerstmann <mg@bytepark.de>
2014-09-22 14:14:24 +0200

    add site adbinbar and navigation prototypes

---
+++ app/admin/modules/site-adminbar.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mg
+ * Date: 22.09.2014
+ * Time: 03:36
+ */
+?>
+<site-adminbar>
+    <ul role="navigation">
+        <li>
+            <a href="#">Dashboard</a>
+        </li>
+        <li>
+            <a href="#" aria-haspopup="true">Benutzer</a>
+            <ul>
+                <li>
+                    <a href="#">Benutzerdaten bearbeiten</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</site-adminbar>
---
+++ app/admin/modules/site-nav.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mg
+ * Date: 22.09.2014
+ * Time: 03:30
+ */
+?>
+
+<nav role="navigation">
+    <ul>
+        <li>
+            <a href="#">Dashboard</a>
+        </li>
+        <li>
+            <a href="#">Logout</a>
+        </li>
+    </ul>
+</nav>
