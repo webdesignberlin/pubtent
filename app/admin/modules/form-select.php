2fc23ba0c91bf3b05dd252d2fd0f184222e363cd
Michael Gerstmann <mg@bytepark.de>
2014-09-22 14:13:39 +0200

    add select filter module

---
+++ app/admin/modules/form-select.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mg
+ * Date: 22.09.2014
+ * Time: 04:06
+ */
+?>
+
+<form>
+    <fieldset>
+
+        <legend>Filterung</legend>
+        <label for="select-field">Auswahl treffen</label>
+        <select name="select-field" id="select-field">
+            <option value="Auswahl 1">Auswahl 1</option>
+            <option value="Auswahl 2">Auswahl 2</option>
+        </select>
+
+        <label for="select-field-submit">anwenden</label>
+        <input type="submit" name="select-field-submit" id="select-field-submit" value="Filtern">
+
+    </fieldset>
+</form>
