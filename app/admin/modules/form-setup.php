a8db010132736f992912a6425e09d5dc5b15a8b1
webdesignberlin <reg@seiler-gerstmann.de>
2014-09-24 02:11:18 +0200

    Update form-setup.php

--- app/admin/modules/form-setup.php
+++ app/admin/modules/form-setup.php
@@ -13,8 +13,6 @@
         <label for="setup-db-databasename">Datenbankname</label>
         <input type="text" name="setup-db-databasename" id="setup-db-databasename" required="required">
 
-
         <input type="submit" name="setup-db-submit" id="setup-db-submit" value="Weiter">
-
     </fieldset>
 </form>
