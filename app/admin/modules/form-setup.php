790e289f4296a789c79df6e569f17ee095dccd38
Michael Gerstmann <mg@bytepark.de>
2014-09-25 09:57:00 +0200

    editd setup form

--- app/admin/modules/form-setup.php
+++ app/admin/modules/form-setup.php
@@ -13,6 +13,15 @@
         <label for="setup-db-databasename">Datenbankname</label>
         <input type="text" name="setup-db-databasename" id="setup-db-databasename" required="required">
 
+        <label for="setup-db-databaseuser">Datenbankbenutzer</label>
+        <input type="text" name="setup-db-databaseuser" id="setup-db-databaseuser" required="required">
+
+        <label for="setup-db-databasepassword">Datenbankpasswort</label>
+        <input type="text" name="setup-db-databasepassword" id="setup-db-databasepassword" required="required">
+
+        <label for="setup-db-databasehost">Datenbank Host</label>
+        <input type="text" name="setup-db-databasehost" id="setup-db-databasehost" required="required">
+
         <input type="submit" name="setup-db-submit" id="setup-db-submit" value="Weiter">
     </fieldset>
 </form>
